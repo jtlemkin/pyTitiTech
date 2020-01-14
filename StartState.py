6ba353d51dbfec74542a18a478b459bed517a533
jtlemkin <jtlemkin@gmail.com>
2020-01-14 14:14:37 -0800

    implemented add subject

--- StartState.py
+++ StartState.py
@@ -1,12 +1,16 @@
 from state import State
 from button import Button
+from fileManager import FileManager
 
-from psychopy import visual
-from psychopy import event
+from psychopy import visual, event, gui
 
 class StartState(State):
     
     context = None
+    fileManager = FileManager("subject-data.csv")
+    
+    subjectIDs = []
+    subjectData = fileManager.read_subject_data()
     
     def __init__(self, win: visual.Window) -> None:
         self.addSubjectButton = Button(win, pos=(0, 0.5), size=(1, 0.5), text="Add Subject")
@@ -25,6 +29,26 @@ class StartState(State):
         
     def handle_input(self) -> None:
         if self.addSubjectButton.has_been_clicked():
-            print("click!")
+            self.addSubject()
+    
+    def addSubject(self) -> None:
+        #762 is temp value for max diameter size
+        subjectInfo = {"ID": "", "Condition": "", "Diameter" : 762}
+        
+        subjectDlg = gui.DlgFromDict(subjectInfo, sortKeys=False, fixed=['Diameter'])
             
-        event.clearEvents()
+        if subjectDlg.OK:
+            if self.isValidSubjectID(subjectInfo):
+                self.fileManager.write_subject_data(list(subjectInfo.values()))
+                
+                self.subjectIDs.append(subjectInfo["ID"])
+                    
+                 #this might not work
+                self.subjectData.append(list(subjectInfo.values()))
+            else:
+                print("invalid subject")
+                 
+    def isValidSubjectID(self, subjectInfo):
+        idIsLongEnough = len(subjectInfo['ID']) >= 4
+        
+        return idIsLongEnough and subjectInfo['ID'] not in self.subjectIDs       
